6dd31694e2cb0c775dcab1caf69e827bbe2ed231
DaehyunJoo <dd70296@gmail.com>
2021-02-24 04:14:16 +0900

    add main-view

---
+++ components/page-view/main.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="page-view-root">
+    <card
+      v-for="(todo, idx) in todoList"
+      :key="todo.id"
+      :todo="todo"
+      @switchFinishFlag="$emit('switchFinishFlag', idx)"
+    ></card>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+import Card from "@/components/component/card.vue";
+import TodoItem from "@/model/todo-item";
+
+@Component({
+  components: {
+    Card
+  }
+})
+export default class PageViewMain extends Vue {
+    @Prop({ type: Array, required: true, default: () => { return [] } })
+    readonly todoList!:Array<TodoItem>
+
+    handle (e: TodoItem, idx: number) {
+      this.$emit('update:todo', idx, e);
+    }
+}
+</script>
+
+<style>
+</style>
