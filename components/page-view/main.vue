6f97331d9cb8398bf4f3c4813811d472a6265e5b
DaehyunJoo <dd70296@gmail.com>
2021-02-24 21:01:37 +0900

    add style between card

--- components/page-view/main.vue
+++ components/page-view/main.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="page-view-root">
     <card
+      class="card"
       v-for="(todo, idx) in todoList"
       :key="todo.id"
       :todo="todo"
@@ -10,24 +11,37 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator'
+import { Vue, Component, Prop } from "vue-property-decorator";
 import Card from "@/components/component/card.vue";
 import TodoItem from "@/model/todo-item";
 
 @Component({
   components: {
-    Card
-  }
+    Card,
+  },
 })
 export default class PageViewMain extends Vue {
-    @Prop({ type: Array, required: true, default: () => { return [] } })
-    readonly todoList!:Array<TodoItem>
+  @Prop({
+    type: Array,
+    required: true,
+    default: () => {
+      return [];
+    },
+  })
+  readonly todoList!: Array<TodoItem>;
 
-    handle (e: TodoItem, idx: number) {
-      this.$emit('update:todo', idx, e);
-    }
+  handle(e: TodoItem, idx: number) {
+    this.$emit("update:todo", idx, e);
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.page-view-root {
+  & > .card {
+    & + .card {
+      @apply mt-4;
+    }
+  }
+}
 </style>
